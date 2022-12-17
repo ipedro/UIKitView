826d90faf5ce5802e82805ec939e1933d207a9ba
Pedro Almeida <ip4dro@gmail.com>
2022-12-17 18:24:48 +0100

    Improve example

--- Sources/UIKitView/UIKitView.swift
+++ Sources/UIKitView/UIKitView.swift
@@ -132,39 +132,64 @@ public struct UIKitView<V: UIView>: View {
 struct UIKitView_Previews: PreviewProvider {
     static var previews: some View {
         ScrollView {
-            VStack(spacing: 48) {
+            VStack {
+                UIKitView {
+                    UILabel()
+                } then: {
+                    $0.adjustsFontForContentSizeCategory = true
+                    $0.numberOfLines = 0
+                    $0.attributedText = NSAttributedString(
+                        string: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer",
+                        attributes: [
+                            .underlineStyle: NSNumber(value: 1),
+                            .font: UIFont.preferredFont(forTextStyle: .headline),
+                            .foregroundColor: UIColor.systemBackground
+                        ]
+                    )
+                }
+                .padding()
+                .background(Color.accentColor)
+                .cornerRadius(16)
+                
                 UIKitView {
                     UIButton(type: .roundedRect)
                 } then: {
-                    $0.setTitle("I'm a Button", for: .normal)
-                    $0.backgroundColor = .systemTeal
-                    $0.setTitleColor(.white, for: .normal)
-                    $0.layer.cornerRadius = 12
+                    $0.setTitle("I'm a button", for: .normal)
                 }
-                .padding()
+                .padding(.vertical)
                 
-                UIKitView {
-                    let label = UILabel()
-                    label.numberOfLines = 0
-                    label.text = "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-                    return label
+                HStack {
+                    // Medium Body Regular Text
+                    Text("Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.")
+                        .padding()
+                    
+                    // Should look exactly the same
+                    UIKitView {
+                        UILabel()
+                    } then: {
+                        $0.adjustsFontForContentSizeCategory = true
+                        $0.numberOfLines = 0
+                        $0.text = "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type."
+                    }
+                    .padding()
                 }
                 
                 HStack {
                     UIKitView {
-                        UILabel()
-                    } then: {
-                        $0.numberOfLines = 0
-                        $0.text = "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+                        UIImageView(image: .init(systemName: "checkmark.seal.fill"))
                     }
+                    .aspectRatio(contentMode: .fit)
+                    
+                    // Should look exactly the same
                     UIKitView {
                         UILabel()
                     } then: {
+                        $0.adjustsFontForContentSizeCategory = true
                         $0.numberOfLines = 0
-                        $0.text = "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+                        $0.text = "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type."
                     }
+                    .padding()
                 }
-                
             }
             .padding()
         }
