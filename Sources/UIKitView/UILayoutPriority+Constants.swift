859dde5c9332e0afd46ccffe2fb459649ad53a56
Pedro Almeida <ip4dro@gmail.com>
2022-12-17 13:31:51 +0100

    Rename constant

--- Sources/UIKitView/UILayoutPriority+Constants.swift
+++ Sources/UIKitView/UILayoutPriority+Constants.swift
@@ -1,5 +1,6 @@
 import UIKit
 
-extension UILayoutPriority {
-    static let compressedSizeLevel = UILayoutPriority(950)
+public extension UILayoutPriority {
+    /// Highest value that is not required. Equivalent to 950.
+    static let highestSizeLevel = UILayoutPriority(950)
 }
