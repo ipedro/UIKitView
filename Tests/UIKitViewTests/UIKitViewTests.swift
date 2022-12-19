6c0e6ea4e783bb6ec32741327e2ab5c9250b7213
Pedro Almeida <ip4dro@gmail.com>
2022-12-19 18:18:32 +0100

    Add small snapshot test

--- Tests/UIKitViewTests/UIKitViewTests.swift
+++ Tests/UIKitViewTests/UIKitViewTests.swift
@@ -5,12 +5,21 @@ import SnapshotTesting
 
 final class UIKitViewTests: XCTestCase {
     func testExample() throws {
-        let someView = UIKitView {
-            UILabel()
-        } then: {
-            $0.text = "My text"
-        }
+        let vc1 = UIHostingController(
+            rootView: UIKitView {
+                UILabel()
+            } then: {
+                $0.text = "My text"
+            })
         
-        assertSnapshot(matching: UIHostingController(rootView: someView), as: .image(on: .iPhone13))
+        let vc2 = UIViewController()
+        let label = UILabel()
+        label.text = "My text"
+        vc2.view = label
+        label.backgroundColor = .systemBackground
+        
+        for vc in [vc1, vc2].shuffled() {
+            assertSnapshot(matching: vc, as: .image(on: .iPhone13, perceptualPrecision: 0.97))
+        }
     }
 }
