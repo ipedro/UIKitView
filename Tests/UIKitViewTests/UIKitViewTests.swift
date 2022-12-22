70468c8cc7978755dc5b8249cf5bac169cfbf6bc
Pedro Almeida <ip4dro@gmail.com>
2022-12-22 10:28:12 +0100

    Kickstart unit tests

--- Tests/UIKitViewTests/UIKitViewTests.swift
+++ Tests/UIKitViewTests/UIKitViewTests.swift
@@ -8,15 +8,16 @@ final class UIKitViewTests: XCTestCase {
         let vc1 = UIHostingController(
             rootView: UIKitView {
                 UILabel()
-            } then: {
+            } onChange: {
                 $0.text = "My text"
+                $0.backgroundColor = .red
             })
         
         let vc2 = UIViewController()
         let label = UILabel()
         label.text = "My text"
         vc2.view = label
-        label.backgroundColor = .systemBackground
+        label.backgroundColor = .blue
         
         for vc in [vc1, vc2].shuffled() {
             assertSnapshot(matching: vc, as: .image(on: .iPhone13, perceptualPrecision: 0.97))
